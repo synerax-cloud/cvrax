d0cf388ebe38b1c6b81ab61ed51cde725eda5b91
Shubham <sreshubham28@gmail.com>
2026-03-28 12:58:00 +0530

    Fix: Add missing output block declaration and remove duplicate
    - Added missing 'output "database_connection_name"' declaration
    - Removed duplicate artifact_registry_url output
    - Terraform syntax now valid

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,6 +12,8 @@ output "region" {
   description = "GCP region"
   value       = var.region
 }
+
+output "database_connection_name" {
   description = "Cloud SQL connection name"
   value       = google_sql_database_instance.postgres.connection_name
 }
@@ -24,11 +26,6 @@ output "database_instance_name" {
 output "database_private_ip" {
   description = "Cloud SQL private IP address"
   value       = google_sql_database_instance.postgres.private_ip_address
-}
-
-output "artifact_registry_url" {
-  description = "Artifact Registry repository URL"
-  value       = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.docker_repo.repository_id}"
 }
 
 output "vpc_connector_name" {
